ee56368d14a0bf64986dfa9a1bd93d251943f3ba
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-08-28 12:15:13 +0700

    update frontend: hero section tablet ui

--- frontend/src/pages/landing/HeroSection.jsx
+++ frontend/src/pages/landing/HeroSection.jsx
@@ -9,10 +9,10 @@ const HeroSection = () => {
         <section aria-label="Hero section" className="relative w-full h-screen flex items-center bg-[url('/hero.png')] bg-cover bg-center gap-14 text-white">
             <div className="inset-0 bg-black opacity-30 absolute" />
             <div className="relative mx-auto max-w-4xl sm:px-0 text-center">
-                <h1 className="text-5xl font-semibold tracking-tight md:text-7xl leading-tight">
+                <h1 className="text-4xl font-semibold tracking-tight md:text-7xl leading-tight">
                     Kedaulatan Pangan, Pendidikan Berkualitas
                 </h1>
-                <p className="mt-8 text-lg font-medium sm:text-xl">
+                <p className="mt-8 text-base font-medium sm:text-xl">
                     Giri Foundation hadir untuk memastikan setiap individu mendapat pangan sehat dan pendidikan inklusif. Melalui pertanian berkelanjutan, beasiswa, dan pemberdayaan masyarakat, kami membangun Indonesia yang mandiri dan sejahtera.
                 </p>
                 <div className="mt-10 flex items-center justify-center gap-x-6">
